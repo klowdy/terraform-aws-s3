967c45478453216301d29f206242c5315d195c16
greenymcduff <chris.greenwood1984@gmail.com>
2020-01-01 12:22:42 +0000

    Fix incorrect variable name

--- policy.tf
+++ policy.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_policy" "custom" {
-  count  = var.create && !var.grant_owner_access && length(var.policy_document) > 0 ? 1 : 0
+  count  = var.create && !var.grant_caller_access ? 1 : 0
   bucket = aws_s3_bucket.this[0].id
   policy = var.policy_document
 
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_policy" "custom" {
 }
 
 resource "aws_s3_bucket_policy" "owner" {
-  count  = var.create && var.grant_owner_access ? 1 : 0
+  count  = var.create && var.grant_caller_access ? 1 : 0
   bucket = aws_s3_bucket.this[0].id
   policy = data.aws_iam_policy_document.owner[0].json
 
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_policy" "owner" {
 data "aws_caller_identity" "caller" {}
 
 data "aws_iam_policy_document" "owner" {
-  count = var.create && var.grant_owner_access ? 1 : 0
+  count = var.create && var.grant_caller_access ? 1 : 0
 
   source_json = var.policy_document
 
